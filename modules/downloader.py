6799e560fc17fb639c20185e3e2ef6a0f0e02ae3
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2022-12-02 19:31:31 +1100

    Updated to only download the missing episodes for a podcast

--- modules/downloader.py
+++ modules/downloader.py
@@ -3,8 +3,8 @@ import feedparser
 import pandas as pd
 import requests
 import validators
-import yt_dlp
 import utils
+import os
 
 
 def download_from_RSS(rss_url: str) -> None:
@@ -16,23 +16,35 @@ def download_from_RSS(rss_url: str) -> None:
     # Parse the RSS feed
     feed = feedparser.parse(rss_url)
 
-    # Save metadata to JSON and CSV files
+    # Create directory for feed if it doesn't already exist
+    directory = utils.slugify(feed.feed.title)
+    os.makedirs(directory, exist_ok=True)
+    os.makedirs(f'{directory}/audio', exist_ok=True)
+
+    # Compare metadata with previous revision
     data = pd.DataFrame(feed.entries)
-    data.to_csv('podcast.csv', index=False)
-    with open('feed.json', 'w') as f:
+    if os.path.exists(f'{directory}/podcast.csv'):
+        prev_data = pd.read_csv(f'{directory}/podcast.csv')
+        entries = pd.concat([data, prev_data]).drop_duplicates(keep=False)
+    else:
+        entries = data
+
+    # Save metadata to JSON and CSV files
+    data.to_csv(f'{directory}/podcast.csv', index=False)
+    with open(f'{directory}/feed.json', 'w') as f:
         json.dump(feed.feed, f)
 
     # Download each podcast in the feed
-    for entry in feed.entries:
+    for entry in entries:
         # Get the podcast URL
-        podcast_url = entry.enclosures[0].href
+        podcast_url = next(x for x in entry['links'] if x['rel'] == 'enclosure').href
 
         # Download the podcast
         response = requests.get(podcast_url)
 
         # Save the podcast to a file
         file_name = utils.slugify(entry.title)
-        with open(file_name + '.mp3', 'wb') as f:
+        with open(f'{directory}/audio/{file_name}.mp3', 'wb') as f:
             f.write(response.content)
 
 
